a002b424c7633214f4e816eb052c5325e8c90156
Sai Pavan Bonam <138384866+SaipavanBonam@users.noreply.github.com>
2024-08-07 12:24:24 +0530

    Update index.js

--- frontend/src/redux/index.js
+++ frontend/src/redux/index.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSliceReducer from "./userSlice";
 import productSlideReducer from "./productSlide";
+import {react} from "react-dom"
 
 export const store = configureStore({
   reducer: {
